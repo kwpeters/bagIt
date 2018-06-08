a6b352b8955bd7804a68b90a68ad94a76a61ff73
Kevin Peters <kwpeters@gmail.com>
2018-06-08 17:09:06 -0400

    Started adding components and some routing.

---
+++ src/app/login.guard.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import {LoginService} from "./login.service";
+
+@Injectable({
+                providedIn: 'root'
+            })
+export class LoginGuard implements CanActivate
+{
+    private _loginService: LoginService;
+
+    public constructor(loginService: LoginService)
+    {
+        this._loginService = loginService;
+    }
+
+    canActivate(
+        // next: ActivatedRouteSnapshot,
+        // state: RouterStateSnapshot
+    ): Observable<boolean> | Promise<boolean> | boolean
+    {
+        return true;
+    }
+}
