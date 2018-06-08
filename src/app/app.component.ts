92acc68958ecbb17a7b343a738be3bebbc146864
Kevin Peters <kwpeters@gmail.com>
2018-06-08 10:57:39 -0400

    Added Firebase initialization.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bagit';
+
+    // region Data Members
+    private _appName = 'Bagit';
+    // endregion
+
+    public constructor() {
+        // Initialize Firebase
+        const config = {
+            apiKey: 'AIzaSyAz94XC84SMI931J1whgZvd-DshZ0KiO8U',
+            authDomain: 'bagit-e8c02.firebaseapp.com',
+            databaseURL: 'https://bagit-e8c02.firebaseio.com',
+            projectId: 'bagit-e8c02',
+            storageBucket: 'bagit-e8c02.appspot.com',
+            messagingSenderId: '147809261454'
+        };
+        firebase.initializeApp(config);
+    }
+
+
+    public get appName() {
+        return this._appName;
+    }
 }
