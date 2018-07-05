05305b9af4827e133e582418e55035e0eafeaf28
Kevin Peters <kwpeters@gmail.com>
2018-07-05 09:19:24 -0400

    Updated documentation.

--- src/app/bagit-model.service.ts
+++ src/app/bagit-model.service.ts
@@ -26,18 +26,33 @@ export class BagitModelService
     }
 
 
+    /**
+     * Updates or inserts the specified user information into the database
+     * @param user - The Firebase user containing information about the user
+     * @return A Promise that is resolved once the operation completes
+     */
     public upsertUser(user: firebase.User): Promise<void>
     {
         return this._refRoot.child("users").child(user.uid).update({
             displayName: user.displayName,
             photoUrl:    user.photoURL
         })
+        // TODO: Get rid of the following prototype code.
         .then(() => {
             this.createDatabase(user, "db1", "one");
         });
     }
 
 
+    /**
+     * Creates a new database that can be access by the specified user.  The
+     * user should already be created.
+     * @param user - The user that is creating this database.  It is assumed
+     * that this user has already been created (see upsertUser()).
+     * @param dbName - The name of the new database
+     * @param description - A description of the new database
+     * @return A Promise that is resolved once the operation completes
+     */
     public createDatabase(user: firebase.User, dbName: string, description: string): Promise<void>
     {
         // TODO: Do a single update() here as explained in this doc.
